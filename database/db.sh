59ac641b33cb5171ca482765b538b21a4c76f155
Harsha JS <selviharsha@gmail.com>
2022-12-17 19:11:01 +0000

    DB - Insert,Update,Delete & Select Completed

--- database/db.sh
+++ database/db.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+MYSQL_HOST=localhost
+MYSQL_USERNAME=root
+MYSQL_PASSWORD=password
+
 echo "***---Welcome---***"
 echo "Connecting to db..."
 sleep 3
@@ -14,17 +18,18 @@ options () {
   echo -n "Enter your choice: "
   read choice;
   case $choice in
-   1) echo -e "\nS.no : Name : Age : Teacher : Bus"
+   1) mysql -h $MYSQL_HOST -u $MYSQL_USERNAME -p$MYSQL_PASSWORD -e "USE shell; SELECT * FROM student;" 2>/dev/null 
       options;;
 
    2) echo -e -n "\nEnter Student Name: "
       read s_name;
       echo -n "Enter Student Age: "
-      read s_age;
+      read age;
       echo -n "Enter Teacher Name: "
       read t_name;
       echo -n "Enter Private/School Bus: "
       read bus;
+      mysql -h $MYSQL_HOST -u $MYSQL_USERNAME -p$MYSQL_PASSWORD -e "USE shell; INSERT INTO student (s_name, age, t_name, bus) VALUES ('$s_name', '$age', '$t_name', '$bus');" 2>/dev/null
       echo -e "Inserted Successfully!\n"
       options;;
 
@@ -33,16 +38,18 @@ options () {
       echo -n "Enter Student Name: "
       read s_name;
       echo -n "Enter Student Age: "
-      read s_age;
+      read age;
       echo -n "Enter Teacher Name: "
       read t_name;
       echo -n "Enter Private/School Bus: "
       read bus;
+      mysql -h $MYSQL_HOST -u $MYSQL_USERNAME -p$MYSQL_PASSWORD -e "USE shell;UPDATE student SET s_name='$s_name', age='$age', t_name='$t_name', bus='$bus' WHERE s_no =$s_no;" 2>/dev/null
       echo -e "Updated Successfully!\n"
       options;;
 
    4) echo -e -n "\nEnter Student S.no to delete: "
       read s_no;
+      mysql -h $MYSQL_HOST -u $MYSQL_USERNAME -p$MYSQL_PASSWORD -e "USE shell;DELETE FROM student WHERE s_no=$s_no;" 2>/dev/null
       echo -e "Record deleted successfully!!!\n"
 	   options;;
 
